d9f4d22d46d509fa1f3d9e0af23ca0ccc6b6c9e7
Aloysius Kang <aloysius.kang1103@gmail.com>
2025-10-28 15:04:38 +0700

    Feature: Added actual weather info from API to weather info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,91 @@
-import Image from "next/image";
+"use client";
+
+import { fetchWeatherApi } from "openmeteo";
+import { useEffect, useState } from "react";
+import {format} from "date-fns";
+
+type WeatherInfo = {
+  current_date: string;
+  temperature: number;
+  apparent_temperature: number;
+  relative_humidity: number;
+  wind_speed: number;
+  precipitation: number;
+  weather_code: number;
+};
 
 export default function Home() {
+  const params = {
+    latitude: -6.1818,
+    longitude: 106.8223,
+    daily: "weather_code",
+    current: [
+      "temperature_2m",
+      "apparent_temperature",
+      "relative_humidity_2m",
+      "wind_speed_10m",
+      "precipitation",
+    ],
+    timezone: "auto",
+    forecast_days: 1,
+  };
+  const url = "https://api.open-meteo.com/v1/forecast";
+
+  const [weatherInfo, setWeatherInfo] = useState<WeatherInfo>();
+
+  // const getWeatherCode = (code:number) => {
+  //   let weatherDescription:string = "";
+  //   if (code === 0) {
+  //    weatherDescription = "Clear sky"
+  //   }else if(code === 1 || code === 2 || code === 3){
+  //     weatherDescription = "Mainly clear, partly cloudy, and overcast"
+  //   }else if(code === 45 || code === 48){
+  //     weatherDescription = "Fog and depositing rime fog"
+  //   }else if(code === 51 || code === 53 || code === 55){
+  //     weatherDescription = "Drizzle: Light, moderate, and dense intensity"
+  //   }
+
+  //   return weatherDescription;
+  // }
+
+  const getWeatherInfo = async (url: string, params: Object) => {
+    const res = await fetchWeatherApi(url, params);
+
+    // Process first location. Add a for-loop for multiple locations or weather models
+    const response = res[0];
+    const utcOffsetSeconds = response.utcOffsetSeconds();
+    const current = response.current()!;
+    const daily = response.daily()!;
+    const currDate = new Date((Number(current.time()) + utcOffsetSeconds) * 1000);
+    const weatherArray = daily.variables(0)!.valuesArray()
+
+    const weatherData : WeatherInfo = {
+      current_date: format(currDate, "EEEE, MMM d yyyy"),
+      temperature: Math.round(current.variables(0)!.value()),
+      apparent_temperature: Math.round(current.variables(1)!.value()),
+      relative_humidity: Math.round(current.variables(2)!.value()),
+      wind_speed: Math.round(current.variables(3)!.value() * 100) / 100,
+      precipitation: Math.round(current.variables(4)!.value() * 100) / 100,
+      weather_code: weatherArray!["0"],
+    };
+    // console.log(
+    //   `\nCurrent date: ${weatherData.current_date}`,
+    //   `\nCurrent temperature_2m: ${weatherData.temperature}`,
+    //   `\nCurrent apparent_temperature: ${weatherData.apparent_temperature}`,
+    //   `\nCurrent wind_speed_10m: ${weatherData.wind_speed}`,
+    //   `\nCurrent relative_humidity_2m: ${weatherData.relative_humidity}`,
+    //   `\nCurrent precipitation: ${weatherData.precipitation}`
+    // );
+    // console.log("\nDaily data", weatherData.weather_code);
+
+    setWeatherInfo(weatherData)
+  };
+
+  useEffect(() => {
+    getWeatherInfo(url, params);
+    return () => {};
+  }, []);
+
   return (
     <section className="home">
       <section className="cta">
@@ -21,29 +106,29 @@ export default function Home() {
       <section className="weather-info">
         <div className="weather-info__container col-span-full">
           <div className="">
-            <h2 className="text-preset-4">Berlin, Germany</h2>
-            <p className="text-preset-6">Tuesday, Aug 5 2025</p>
+            <h2 className="text-preset-4">Jakarta, Indonesia</h2>
+            <p className="text-preset-6">{weatherInfo?.current_date}</p>
           </div>
           <div className="weather-info__temp">
             <img src="/assets/images/icon-sunny.webp" alt="" />
-            <p className="text-preset-1">20</p>
+            <p className="text-preset-1">{weatherInfo?.temperature}</p>
           </div>
         </div>
         <div className="weather-info__small-container">
           <p className="text-preset-6">Feels Like</p>
-          <p className="text-preset-3">18</p>
+          <p className="text-preset-3">{weatherInfo?.apparent_temperature}</p>
         </div>
         <div className="weather-info__small-container">
           <p className="text-preset-6">Humidity</p>
-          <p className="text-preset-3">46%</p>
+          <p className="text-preset-3">{weatherInfo?.relative_humidity}%</p>
         </div>
         <div className="weather-info__small-container">
           <p className="text-preset-6">Wind</p>
-          <p className="text-preset-3">14 km/h</p>
+          <p className="text-preset-3">{weatherInfo?.wind_speed} km/h</p>
         </div>
         <div className="weather-info__small-container">
           <p className="text-preset-6">Precipitation</p>
-          <p className="text-preset-3">0 mm</p>
+          <p className="text-preset-3">{weatherInfo?.precipitation} mm</p>
         </div>
       </section>
       <section className="daily-forecast">
